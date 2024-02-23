7a7d42033ee642ee080f6064e35a5503ab003211
Jake Prentice <prenticejake@me.com>
2024-02-23 15:41:24 +0000

    changed maps to always google maps

--- src/FirebaseConfig.ts
+++ src/FirebaseConfig.ts
@@ -1,7 +1,11 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp} from "firebase/app";
 import {getAuth, initializeAuth} from "firebase/auth";
+import {getFirestore} from "firebase/firestore";
 import AsyncStorage from '@react-native-async-storage/async-storage';
+
+//TODO - enable offline support 
+//https://firebase.google.com/docs/firestore/manage-data/enable-offline?authuser=0
 
 const firebaseConfig = {
   apiKey: "AIzaSyASGr7s0BoK7iZXqpG93m2vzZ7cJSo1fnw",
@@ -22,3 +26,4 @@ export const FIREBASE_APP = initializeApp(firebaseConfig);
 
   
 export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
+export const db = getFirestore(FIREBASE_APP);
--- src/screens/MapScreen/index.tsx
+++ src/screens/MapScreen/index.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
+
+//example long+lat: 51.371989, -2.341385
 
 const Map = () => {
-    return <MapView style={styles.map} />;
+    return (
+        <MapView 
+            provider={PROVIDER_GOOGLE}
+            style={styles.map}>
+            <Marker
+                coordinate={{
+                    latitude: 51.371989,
+                    longitude: -2.341385,
+                }}
+                title="Bath"
+                description="some location in Bath, UK"
+            />
+        </MapView>
+    )
 }
 
 const MapScreen = () => {
     return (
-        // <View style={styles.container}>
-        //     <Text style={styles.title}>Welcome to the Map Screen!</Text>
-        // </View>
         <Map />
     );
 };
