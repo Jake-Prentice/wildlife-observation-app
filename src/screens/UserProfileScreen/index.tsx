67d89035ea2cc38d13491af7d1be4071abe24ee2
perch2 <perch12342@gmail.com>
2024-03-10 07:13:19 +0700

    Profile + fixing

--- src/screens/UserProfileScreen/index.tsx
+++ src/screens/UserProfileScreen/index.tsx
@@ -1,14 +1,29 @@
+import { db } from '@/FirebaseConfig';
 import { useUser } from '@/contexts/UserContext';
+import { UserStatSchema } from '@/services/schemas';
 import { Button, ButtonText} from '@gluestack-ui/themed';
+import { User } from 'firebase/auth';
+import { doc } from 'firebase/firestore';
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import styles from './style';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 const UserProfileScreen = () => {
     const user = useUser(); 
+    const data = user.stats ? Object.keys(user.stats) : []
+    
+    const renderItem = ({item}: {item:string}) => {
+    return <View style={styles.statBox}>
+        <Text style={styles.stattext}>{item}</Text>
+        <Text style={styles.stattext}>{user.stats![item as keyof UserStatSchema]}</Text>
+    </View>
+    }
+
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Welcome to the User Profile Screen!</Text>
-            <Button
+            <View style={styles.usernameBox}>
+                <Text style={styles.title}>Logged in as: {user.info?.displayName}</Text>
+                <Button
                 onPress={user.logout}
                 size="md"
                 variant="solid"
@@ -18,20 +33,20 @@ const UserProfileScreen = () => {
                 >
                 <ButtonText>Logout </ButtonText>
             </Button>
+            </View>
+            <Text style={styles.header}>My stats</Text>
+            <FlatList
+            style ={styles.statsList} 
+            data={data}
+            renderItem={renderItem}
+            ItemSeparatorComponent={() => {return <View style={styles.line}/>}}
+            ListHeaderComponent={() => {return <View style={[styles.line,{marginHorizontal:-10}]}/>}}
+            />
+            
         </View>
     );
 };
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    title: {
-        fontSize: 24,
-        fontWeight: 'bold',
-    },
-});
+
 
 export default UserProfileScreen;
